8908883c69ed68d5736f65cb355d76aa747b5c64
Yeslyn <cyeslyn@gmail.com>
2019-09-16 16:56:11 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+import java.io.*;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws IOException{
+
+        String fname;
+
+        //FileWriter fwriter = new FileWriter("names.txt", true);
+        //PrintWriter outfile = new PrintWriter(fwriter);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a file name for output");
+        fname = input.nextLine();
+        PrintWriter outfile = new PrintWriter(fname);
+        String name;
+        String choice = "yes";
+
+
+        while(!(choice.equals("no"))){
+            System.out.println("Enter a name to store in a file");
+            name = input.nextLine();
+            outfile.println(name);
+            System.out.println("Do you want to add more names?");
+            choice = input.nextLine();
+        }
+        outfile.close();
+    }
+}
